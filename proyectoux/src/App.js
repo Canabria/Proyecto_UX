c56c327f7981ca2cf5bca22c1ab660573bfc453a
Canabria <83465674+Canabria@users.noreply.github.com>
2023-12-15 14:40:28 -0600

    app designfinish

--- proyectoux/src/App.js
+++ proyectoux/src/App.js
@@ -9,6 +9,10 @@ function App() {
   const [Pass, setPass] = useState('');
   const [EmailR, setEmailR] = useState('');
   const [PassR, setPassR] = useState('');
+  const [Aloja, setAloja] = useState(true);
+  const [Fav, setFav] = useState(false);
+  const [Res, setRes] = useState(false);
+  const [FavT, setFavT] = useState('');
   const handleInputChange = (e) => {
     setEmail(e.target.value);
   };
@@ -20,6 +24,9 @@ function App() {
   };
   const handleInputChange3 = (e) => {
     setPassR(e.target.value);
+  };
+  const handleInputChange4 = (e) => {
+    setFavT(e.target.value);
   };
   const Log = () => {
     setreg(false);
@@ -36,6 +43,23 @@ function App() {
     setreg(false);
     setEmailR("");
     setPassR("");
+  };
+  const Alojamiento = () => {
+    setFav(false);
+    setFavT("");
+    setRes(false);
+  };
+  const Favoritos = () => {
+    setAloja(false);
+    setFavT("");
+    setFav(true);
+    setRes(false)
+  };
+  const Reservados = () => {
+    setAloja(false);
+    setFavT("");
+    setFav(false);
+    setRes(true)
   };
   return (
     <div className="App">
@@ -67,7 +91,35 @@ function App() {
              }
           </section> 
           ): (<section className='APP'>
-          <h1>Bienvenido al APP</h1>
+          <h1>Trivago logo<button className="btn-logout" onClick={RegsFinal}>Log out</button></h1>
+          <section className='main_page'>
+             /* aqui es donde va email de usuario*/
+             <h2>Email del usuario</h2>
+             <p><button className="btn-submit" onClick={Alojamiento}>Alojamiento</button>
+             <button className="btn-submit" onClick={Favoritos}>Favoritos</button>
+             <button className="btn-submit" onClick={Reservados}>Alojamientos reservados</button></p>
+             {
+              Aloja &&
+                <segment className= 'Alojamiento'>
+                  /* aqui va el map  el contiene un imagen el que se las proporciono la locacion del habitacion , el tipo y se esta resarvado o no*/
+                </segment>
+              
+             }
+             {
+              Fav &&
+              <segment className= 'Favoritos' >
+                /* aqui tiene textboxes para que cual el usuario ingresar Alojamiento favorito y se buscar todos los alojamientos disponibles de ese tipo se proyecta por medio un map lo mismo que alojamientos pero ahora se les agrega un boton para hacer las reservas   */
+                <p>Ingrese el tipo de alojamiento: <input type="text" className='form-input' placeholder='Ingrese de su tipo' value={FavT} onChange={handleInputChange4} /><button className="Search" >Favoritos</button></p>
+              </segment>
+             }
+             {
+              Res && 
+              <segment className= 'Resarvados' >
+                /*  lo mismo que alojamientos solo que tienes que proyectar un map el cual enseña los que sean reservados por el usuario  */
+              </segment>
+             }
+          </section>
+
        </section> )
           
           
