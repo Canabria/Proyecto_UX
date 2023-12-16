7e60d41cb18e4c8d8432b37143447ffb78a7defe
emicantarero <93396886+emicantarero@users.noreply.github.com>
2023-12-15 22:42:31 -0600

    Backend consolidado

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* eslint-disable quotes *//* eslint-disable prettier/prettier */
+/*eslint-disable quotes *//* eslint-disable prettier/prettier */
 const express = require('express');
 const path = require('path'); 
 const {initializeApp} = require("firebase/app");
@@ -9,7 +9,7 @@ const {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sign
 
 //MONGODB
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://emicantarero:Hernandez09@examenux.aotz8sy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jafetmorel:HsUeQo5lPEBkhkDe@proy.ujf1bdk.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -201,3 +201,55 @@ servidor.post('/createCliente', async (req, res)=>{
       await client.close();
   }
 })
+
+servidor.get('/getAirbnb', async (req, res) => {
+  try {
+    const client = new MongoClient(uri);
+    const mainDB = client.db("test");
+    const Post = mainDB.collection("airbnb");
+    const query = {};
+    const options = {
+      projection: { _id: 0, pais: 1, ciudad: 1, calle: 1, numero: 1, currentOwner: 1 },
+    };
+    const cursor = Post.find(query, options);
+    let arr = []
+    for await (const doc of cursor) {
+      console.dir(doc);
+      arr.push(doc)
+    }
+    res.status(200).send({
+      documentos: arr,
+    });
+
+  } catch (error) {
+    res.status(500).send("No se pudo ejecutar la query...")
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+})
+
+servidor.put('/updateAirbnb',async (req,res)=>{
+  try {
+    const client = new MongoClient(uri);
+    const database = client.db("test");
+    const usuarios = database.collection("airbnb");
+    const filter = { calle: req.body.calle, numero: req.body.numero};
+    const options = { upsert: false };
+    const updateDoc = {
+      $set: {
+        currentOwner: req.body.currentOwner
+      },
+    };
+    const result = await usuarios.updateOne(filter, updateDoc, options);
+    console.log(
+      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+    );
+    res.status(200).send("Se actualizo la informacion correctamente");
+  }catch (error){
+    res.status(500).send("No se pudo actualizar la información")
+  } finally {
+    await client.close();
+  }
+   
+} )
